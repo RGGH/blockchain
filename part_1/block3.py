fd4d9d289273da28359c83a0460fa75b8cff1b11
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-09-24 10:41:17 +0100

    Create block3.py

---
+++ part_1/block3.py
@@ -0,0 +1,24 @@
+'''A blockchain is a growing list of records, called blocks, that are linked together using
+cryptography. Each block contains a cryptographic hash of the previous block, a
+timestamp, and transaction data.'''
+
+# hashes in the bitcoin are protocol typically computed twice
+import hashlib
+d = hashlib.sha256(b"hello")
+d2 = hashlib.sha256()
+d.hexdigest()
+d2.update(d.digest())
+d2.hexdigest()
+print(d2.digest())
+
+# https://en.bitcoin.it/wiki/Protocol_documentation#Hashes
+# https://en.bitcoin.it/wiki/Network
+# https://en.bitcoin.it/wiki/Help:FAQ#What_is_Bitcoin.3F
+
+# Why are hashes in the bitcoin protocol typically computed twice (double computed)?
+# https://en.wikipedia.org/wiki/Length_extension_attack
+# https://ctftime.org/writeup/30223
+
+
+from datetime import datetime
+timestamp_0 = datetime.timestamp(datetime.fromisoformat('2011-11-04 00:05:23.111'))
